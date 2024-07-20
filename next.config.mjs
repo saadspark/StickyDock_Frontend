c2ae5260cdf099fe99ce0df4ec45f8ccd28d8b92
saadspark <saadahmadsaprk@gmail.com>
2024-07-20 13:29:53 +0500

    upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',  // Ensure that Next.js exports static HTML
-    distDir: 'out',  // Output directory
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  distDir: 'out', // Output directory
+};
+
+export default nextConfig;
